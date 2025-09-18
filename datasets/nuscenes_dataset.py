39dbe069ef4a6401ad1bb579541e504db76d6137
TahaRasouli <149691138+TahaRasouli@users.noreply.github.com>
2025-09-18 18:49:33 +0200

    fixed a bug!

--- datasets/nuscenes_dataset.py
+++ datasets/nuscenes_dataset.py
@@ -4,7 +4,7 @@ from typing import Callable, Dict, List, Optional, Tuple, Union
 import glob
 import numpy as np
 import torch
-from torch_geometric.data import Data, Dataset, DataEdgeAttr
+from torch_geometric.data import Data, Dataset
 from tqdm import tqdm
 from nuscenes.nuscenes import NuScenes
 from nuscenes.map_expansion.map_api import NuScenesMap
@@ -42,7 +42,7 @@ class NuScenesDataset(Dataset):
         # Safe load with TemporalData allowlist
         self.data_list: List = []
         for f in self.pt_files:
-            with torch.serialization.safe_globals([TemporalData, DataEdgeAttr]):
+            with torch.serialization.safe_globals([TemporalData, Data]):
                 self.data_list.append(torch.load(f))
 
         super().__init__(root, transform)
