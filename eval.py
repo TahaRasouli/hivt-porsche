684e400a531697eb965d9fcf82c06d03b8465b8e
TahaRasouli <149691138+TahaRasouli@users.noreply.github.com>
2025-09-24 09:30:58 +0200

    Addressed the Namespace issue!

--- eval.py
+++ eval.py
@@ -1,22 +1,9 @@
-# Copyright (c) 2022, Zikang Zhou. All rights reserved.
-#
-# Licensed under the Apache License, Version 2.0 (the "License");
-# you may not use this file except in compliance with the License.
-# You may obtain a copy of the License at
-#
-#     http://www.apache.org/licenses/LICENSE-2.0
-#
-# Unless required by applicable law or agreed to in writing, software
-# distributed under the License is distributed on an "AS IS" BASIS,
-# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-# See the License for the specific language governing permissions and
-# limitations under the License.
 from argparse import ArgumentParser
 
 import pytorch_lightning as pl
 from torch_geometric.data import DataLoader
 
-from datasets import ArgoverseV1Dataset
+from datasets.argoverse_v1_dataset import ArgoverseV1Dataset
 from models.hivt import HiVT
 
 if __name__ == '__main__':
@@ -32,9 +19,31 @@ if __name__ == '__main__':
     parser.add_argument('--ckpt_path', type=str, required=True)
     args = parser.parse_args()
 
-    trainer = pl.Trainer(args)
+    # Lightning 2.x: manually configure trainer
+    trainer = pl.Trainer(
+        accelerator='gpu' if args.gpus > 0 else 'cpu',
+        devices=args.gpus if args.gpus > 0 else None,
+        num_sanity_val_steps=0
+    )
+
+    # Load model from checkpoint
     model = HiVT.load_from_checkpoint(checkpoint_path=args.ckpt_path, parallel=True)
-    val_dataset = ArgoverseV1Dataset(root=args.root, split='val', local_radius=model.hparams.local_radius)
-    dataloader = DataLoader(val_dataset, batch_size=args.batch_size, shuffle=False, num_workers=args.num_workers,
-                            pin_memory=args.pin_memory, persistent_workers=args.persistent_workers)
+
+    # Load preprocessed validation dataset (skip any raw data processing)
+    val_dataset = ArgoverseV1Dataset(
+        root=args.root,
+        split='val',
+        local_radius=model.hparams.local_radius
+    )
+
+    dataloader = DataLoader(
+        val_dataset,
+        batch_size=args.batch_size,
+        shuffle=False,
+        num_workers=args.num_workers,
+        pin_memory=args.pin_memory,
+        persistent_workers=args.persistent_workers
+    )
+
+    # Run evaluation
     trainer.validate(model, dataloader)
